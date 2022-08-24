0cffb145f303ad44c044a20fd49166d72d572b04
Kyle Taylor <ktkage80uk@gmail.com>
2022-08-24 22:04:33 +0100

    added new components,routing and authguard

---
+++ client/src/app/_guards/auth.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AccountService } from '../_services/account.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private accountService: AccountService, private toastr: ToastrService) {}
+
+  canActivate(): Observable<boolean> {
+    return this.accountService.currentUser$.pipe( //Set up pipe and use the account service observable
+      map(user => {
+        if (user) return true; // if we have a user return true
+        this.toastr.error('You shall not pass') //else return error
+      })
+    )
+  }
+  
+}
