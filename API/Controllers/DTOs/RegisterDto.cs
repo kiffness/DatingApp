94ef5fb85fff5878e154c33467fe33a7ee90d74e
Kyle Taylor <ktkage80uk@gmail.com>
2022-08-31 20:04:54 +0100

    Added Error handling

--- API/Controllers/DTOs/RegisterDto.cs
+++ API/Controllers/DTOs/RegisterDto.cs
@@ -12,6 +12,7 @@ namespace API.Controllers.DTOs
         public string UserName { get; set; }
         
         [Required]
+        [StringLength(8, MinimumLength = 4)]
         public string Password { get; set; }
     }
 }
---
+++ API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+using API.Errors;
+
+namespace API.Middleware
+{
+    //Error handling middleware
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next; //magic
+        private readonly ILogger<ExceptionMiddleware> _logger; //our logger
+        private readonly IHostEnvironment _env; //check env, if dev or prod
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
+        {
+            _env = env;
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context); //throw error down middle ware
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError(ex, ex.Message); //log our exception
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+
+                var response = _env.IsDevelopment()
+                    ? new ApiException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString())
+                    : new ApiException(context.Response.StatusCode, "Internal Server Error");
+
+                var options = new JsonSerializerOptions{PropertyNamingPolicy = JsonNamingPolicy.CamelCase};
+
+                var json = JsonSerializer.Serialize(response, options);
+
+                await context.Response.WriteAsync(json);
+            }
+        }
+    }
+}
